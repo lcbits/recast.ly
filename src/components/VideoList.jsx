37e86c1e1e72a103427d738f6b946c51c930f6c7
lcbits <lynnc24@gmail.com>
2016-05-06 16:40:04 -0700

    Pass VideoList specs

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,17 +1,18 @@
-var VideoList = () => (
-  <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-  </div>
-);
+var VideoList = props => {
+  // constructor () {
+  //   this.props.videos = props.videos;
+  // },
 
+  //render() {
+  return (
+    <div className="video-list media">
+      {props.videos.map(video =>
+        <VideoListEntry video={video}/>
+      )}
+    </div>
+  );
+  //}
+};
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
@@ -21,3 +22,8 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+    // <ul>
+    //   {props.videos.map(video =>
+    //     <VideoListEntry video={video}/>
+    //   )}
+    // </ul>
