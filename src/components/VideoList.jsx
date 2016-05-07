7c43bb8bab5f62017e52a41c459ba803102b11a3
lcbits <lynnc24@gmail.com>
2016-05-06 17:30:00 -0700

    Pass VideoPlayer specs

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,11 @@
-var VideoList = props => {
-  // constructor () {
-  //   this.props.videos = props.videos;
-  // },
+var VideoList = props => (
+  <div className="video-list media">
+    {props.videos.map(video =>
+      <VideoListEntry video={video}/>
+    )}
+  </div>
+);
 
-  //render() {
-  return (
-    <div className="video-list media">
-      {props.videos.map(video =>
-        <VideoListEntry video={video}/>
-      )}
-    </div>
-  );
-  //}
-};
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
