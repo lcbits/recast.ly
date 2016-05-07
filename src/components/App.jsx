8fc51ffb836028712f03f15e68ca8da8171b166b
lcbits <lynnc24@gmail.com>
2016-05-06 20:03:22 -0700

    Make App into a class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={window.exampleVideoData[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={window.exampleVideoData}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      currentVideo: window.exampleVideoData[1]
+    };
+
+    this.updateCurrentVideo = this.updateCurrentVideo.bind(this);
+  }
+
+  updateCurrentVideo(newVid) {
+    this.setState( {currentVideo: newVid} );
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={window.exampleVideoData} clicker={this.updateCurrentVideo} />
+        </div>
+      </div>
+    );
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
